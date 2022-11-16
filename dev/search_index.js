89fee29fc054efcb4aca06cb292c64f15974c105
Documenter.jl <documenter@juliadocs.github.io>
2022-11-16 12:23:30 +0000

    build based on 9b90e67

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BadApples","category":"page"},{"location":"#BadApples","page":"Home","title":"BadApples","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BadApples.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BadApples]","category":"page"}]
+}
